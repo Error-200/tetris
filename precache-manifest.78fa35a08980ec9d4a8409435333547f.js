c1ba195fa161c780e96fefb5c877c17566344356
YASH VARDHAN PRASAD <yashvardhanprasad@YASHs-MacBook-Pro.local>
2020-04-19 14:32:21 +0530

    Updates

---
+++ precache-manifest.78fa35a08980ec9d4a8409435333547f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026683de42db9221f8660ff5602ad0e8",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "c2a353d188914018386f",
+    "url": "/tetris/static/css/main.3e56d105.chunk.css"
+  },
+  {
+    "revision": "1aa9b997fd2c84fe1e89",
+    "url": "/tetris/static/js/2.7201fe05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris/static/js/2.7201fe05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a353d188914018386f",
+    "url": "/tetris/static/js/main.68deca57.chunk.js"
+  },
+  {
+    "revision": "f7b0478d5c8f5e70d8f2",
+    "url": "/tetris/static/js/runtime-main.afcdf205.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris/static/media/bg.2810fe6b.png"
+  },
+  {
+    "revision": "e3edf624dce19f22354d52e94f33b50a",
+    "url": "/tetris/static/media/tetris.e3edf624.mp3"
+  }
+]);
